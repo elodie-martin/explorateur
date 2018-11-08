f5c2929dad57962aa0c555b91dfa8013798a15ff
Valuminous <valerie.h@codeur.online>
2018-11-08 15:46:54 +0100

    mise à jour du scss

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
+<link rel="stylesheet" href="css/style.css" href="css/styles.css">
 <?php
 require 'vendor/autoload.php';
 
 // Routing
 $page = 'index';
+
 if (isset($_GET['p'])) {
     $page = $_GET['p'];
 }
@@ -15,19 +17,32 @@ $twig = new Twig_Environment($loader, [
 ]);
 
 switch ($page) {
-    case 'secondary':
-        echo $twig->render('secondary.twig');
-        break;
     case 'index':
-        echo $twig->render('index.html.twig');
-        break;
-    case 'affichageFichiers':
-        echo $twig->render('affichageFichiers.twig');
+
+        $dir = './';
+        $dh = opendir( $dir );
+        
+       
+        $realpath = realpath($dir);
+
+        $liste = array();
+
+        while (( $file = readdir( $dh)) !== false ){
+            if( is_dir( $file ) ){
+                $liste[] = array("fichier" => $file, "isDir" => true, "extension" => "");
+            }else{
+                $liste[] = array("fichier" => $file, "isDir" => false, "extension" => pathinfo( $dir.$file,PATHINFO_EXTENSION));
+            }
+        }
+
+        closedir( $dh );
+        
+        echo $twig->render('affichageFichiers.twig', array ( 'dir' => $dir, 'liste' => $liste, 'realpath' => $realpath  ));
+       
         break;
     default:
         header('HTTP/1.0 404 not found');
-        echo $twig->render('twig/404.twig');
+        echo $twig->render('404.twig');
         break;
 }
-
 ?>

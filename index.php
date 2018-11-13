07c6ea9ea71bac143dea6f79a746ad8406aa4752
Elodie Martin <elodie.m@codeur.online>
2018-11-13 15:52:40 +0100

    ajout media et navigation

--- index.php
+++ index.php
@@ -3,51 +3,65 @@ require 'vendor/autoload.php';
 
 // Routing
 $page = 'index';
-
-
+$listeDossier = array();
+$listeFichier = array();
 
 // Rendu du template
 $loader = new Twig_Loader_Filesystem('twig');
 $twig = new Twig_Environment($loader, [
-    'cache' => false, // __DID__ . '/tmp'
+'cache' => false, // _DIR_ . '/tmp'
 
 ]);
 
-
 switch ($page) {
-    case 'index':
+case 'index':
 
-        $dir = './';
-        $dh = opendir( $dir );
-        
-       
-        $realpath = realpath($dir);
 
-        $listeDossier = array();
-        $listeFichier = array(); 
-
-        while (( $file = readdir( $dh)) !== false ){
-           
-            if( is_dir( $file ) && $file != '.' && $file != '..' && $file != '.git' && $file != '.sass-cache'){
-            
-                $listeDossier[] = array("fichier" => $file, "isDir" => true, "extension" => "");
-            }else if ($file != '.' && $file != '..' && $file != '.git' && $file != '.sass-cache' && $file != '.gitignore' && $file != 'composer.json' && $file != 'composer.lock'){
-                $listeFichier[] = array("fichier" => $file, "isDir" => false, "extension" => pathinfo( $dir.$file,PATHINFO_EXTENSION));
-            }
-        }
-        closedir( $dh );
-
-        sort($listeDossier);
-        sort($listeFichier);
-        
-        echo $twig->render('affichageFichiers.twig', array ( 'dir' => $dir, 'listeDossier' => $listeDossier, 'listeFichier' => $listeFichier,'realpath' => $realpath,   ));
-       
-        break;
-    default:
-        header('HTTP/1.0 404 not found');
-        echo $twig->render('404.twig');
-        break;
+break;
+default:
+header('HTTP/1.0 404 not found');
+echo $twig->render('404.twig');
+break;
 }
+function dossiers($twig) { 
+if ( (isset($_GET['dossier'])) && (!empty($_GET['dossier']))) {
+$dir = './'.$_GET['dossier'];
+$dh = opendir( $dir );
+$realpath = realpath($dir);
+
+}
+else { 
+$dir = './';
+$dh = opendir( $dir );
+$realpath = realpath($dir);
+}
+$listeDossier = array();
+$listeFichier = array(); 
+
+while (( $file = readdir( $dh)) !== false ){
+
+if( is_dir( $file ) && $file != '.' && $file != '..' && $file != '.git' && $file != '.sass-cache' && $file != 'symfony' && $file != 'erusev' && $file != 'composer'){
+
+$listeDossier[] = array("fichier" => $file, "isDir" => true, "extension" => "");
+}else if ($file != '.' && $file != '..' && $file != '.git' && $file != '.sass-cache' && $file != '.gitignore' && $file != 'composer.json' && $file != 'composer.lock' && $file != 'symfony' && $file != 'erusev' && $file != 'composer'){
+$listeFichier[] = array("fichier" => $file, "isDir" => false, "extension" => pathinfo( $dir.$file,PATHINFO_EXTENSION));
+}
+}
+sort($listeDossier);
+sort($listeFichier);
+closedir( $dh );
+echo $twig->render('affichageFichiers.twig', array ( 'dir' => $dir, 'listeDossier' => $listeDossier, 'listeFichier' => $listeFichier,'realpath' => $realpath, ));
+};
+
+dossiers($twig);
+
+
+
+
+
+
+
+
 
 
 ?>
